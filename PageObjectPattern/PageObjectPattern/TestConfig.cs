03901f87003b7779cf7668cb6757a23458946ca6
Jakub Gwiazda <snox.soft@gmail.com>
2023-11-20 12:42:36 +0100

    Added a TestConfig class which setups the browser context shared between all page objects, and the appsettings json to store securily data such as login credentials

---
+++ PageObjectPattern/PageObjectPattern/TestConfig.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace PageObjectPattern;
+
+public class TestConfig
+{
+    private IPage Page = null!;
+    private string _appURL { get; set; } = null!;
+    private string _username { get; set; } = null!;
+    private string _password { get; set; } = null!;
+    private ILocator _usernameLoginTextField => Page.Locator("id=username_login");
+    private ILocator _passwordLoginTextField => Page.Locator("id=password_login");
+    private ILocator _loginButton => Page.Locator("text=Log in");
+    
+    private static readonly IConfiguration _configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddUserSecrets(Assembly.GetExecutingAssembly())
+        .Build();
+    
+    [SetUp]
+    public async Task SetUpBrowserContext()
+    {
+        var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true,
+            Timeout = 60000
+        });
+        Page = await browser.NewPageAsync();
+    }
+    
+    [SetUp]
+    public void SetUpAppsettingsContext()
+    {
+        _appURL = _configuration["AppURL"]!;
+        _username = _configuration["Credentials:Username"]!;
+        _password = _configuration["Credentials:Password"]!;
+    }
+
+    protected async Task LogInToApplication()
+    {
+        await Page.GotoAsync(_appURL);
+        await _usernameLoginTextField.FillAsync(_username);
+        await _passwordLoginTextField.FillAsync(_password);
+        await _loginButton.ClickAsync();
+    }
+}
